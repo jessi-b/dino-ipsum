1a7114114ae7a1b903dee0a8d906f4679f29b060
Daniel Lindsey <daniellindsey85254@gmail.com>
2022-02-08 13:16:50 -0800

    add dinosaur word class and static method, build HTML
    Co-Authored-by: Jessica Baker <jessb.epicodus@gmail.com>

--- src/index.js
+++ src/index.js
@@ -2,4 +2,39 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-// import *** from './js/*** */.js';
+import DinoFinder from './js/dino-finder.js';
+
+$(document).ready(function() {
+  const dinoArray = [];
+  const outputArray = [];
+  $("#play-game").click(function() {
+    $("#game").show();
+    $("#play-game").hide();
+    let promise = DinoFinder.getDino();
+    promise.then(function(response) {
+      const body = JSON.parse(response);
+      let dinoWord = (body[0][0]).split("");
+      dinoWord.forEach(function(letter) {
+        dinoArray.push(letter);
+        $(".dinoResult").append(`__  &nbsp;&nbsp;&nbsp;&nbsp;`);
+      });
+    }, function(error) {
+      console.log(error);
+    });
+  });
+  $("#submit").click(function() {
+    console.log(dinoArray);
+    
+    let inputLetter = $("#inputLetter").val();
+    let outputString = "";
+    dinoArray.forEach(function(letter) { 
+      if (letter === inputLetter) {
+        outputString += letter;
+      } else {
+        outputString += "__  &nbsp;&nbsp;&nbsp;&nbsp;"
+      }
+    });
+    $(".dinoResult").html(outputString);
+    console.log(outputString);
+  });
+});
